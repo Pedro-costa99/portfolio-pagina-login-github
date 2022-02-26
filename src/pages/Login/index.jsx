0ee0b7a06b20b66e588207f7dbf00b7abfd309c7
Pedro Costa <pedro.costa@satellitus.com>
2022-02-26 12:40:09 -0300

    first commit

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles.module.css';
+import cx from 'classnames'
+
+const Login = () => {
+  return (
+    <div>      
+      <h1>Login</h1>        
+    </div>
+  )
+}
+
+export default Login
